2f62d415089f7bd213370a10a21970ee87431847
sarmini1 <spencerarmini@gmail.com>
2021-06-01 15:23:55 -0700

    styled navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,7 @@
 import { NavLink } from "react-router-dom";
+import Button from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Link, NavDropdown, Brand } from "react-bootstrap";
 
 /** NavBar
  * 
@@ -11,29 +14,26 @@ import { NavLink } from "react-router-dom";
 function NavBar({ currentUser }) {
 
   //comment out currentUser if condition to demo experience for logged in user
-  //if (currentUser) {
-    return (
-      <div className="NavBar">
-      Logged in Navbar
-      <NavLink exact to="/">Jobly</NavLink>
-      <NavLink exact to="/companies">Companies</NavLink>
-      <NavLink exact to="/jobs">Jobs</NavLink>
-      <NavLink exact to="/profile">Profile</NavLink>
-      <NavLink exact to="/logout">Logout</NavLink>
-      </div>
-    );
-  //}
-  //else{
-    return (
-      <div className="NavBar">
-      Logged out  Navbar
-      <NavLink exact to="/">Jobly</NavLink>
-      <NavLink exact to="/login">Login</NavLink>
-      <NavLink exact to="/signup">Signup</NavLink>
-      </div>
-    );
 
-  //}
+  return (
+    < Navbar bg='dark' expand="lg" variant="dark" className="justify-content-between">
+      <Navbar.Brand as={NavLink} to="/">Jobly</Navbar.Brand>
+      {currentUser && <div className="navbar-nav ml-auto">
+        <Nav.Link as={NavLink} to="/companies">Companies</Nav.Link>
+        <Nav.Link as={NavLink} to="/jobs">Jobs</Nav.Link>
+        <Nav.Link as={NavLink} to="/profile">Profile</Nav.Link>
+        <Nav.Link as={NavLink} to="/logout">Logout</Nav.Link>
+      </div>}
+      {!currentUser && <div className="navbar-nav ml-auto">
+        <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
+        <Nav.Link as={NavLink} to="/signup">Signup</Nav.Link>
+      </div>}
+    </Navbar >
+  );
 }
 
 export default NavBar;
+
+//change divs to nav tags
+//specific class names
+//potentially lowercase the B
